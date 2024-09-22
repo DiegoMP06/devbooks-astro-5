e039adb59fd3625856b03e4012271c4f1521e833
Diego Meneses Perez <perezdi060606@gmail.com>
2024-09-22 17:41:45 -0600

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,22 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, envField } from 'astro/config';
+
+import tailwind from '@astrojs/tailwind';
+
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: 'server',
+  integrations: [tailwind()],
+
+  env: {
+    schema: {
+      SHOW_BUY_BUTTON: envField.boolean({access: 'public', context: 'server', default: false}),
+      SCORE_API_ENDPOINT: envField.string({access: 'public', context: 'server', default: ''}),
+      // SCORE_API_ENDPOINT: envField.string({access: 'public', context: 'client', default: ''}),
+    }
+  },
+
+  adapter: vercel(),
+});
