9b6fdefaef2a94d43c051330350139a3ec0bf1df
Diego Meneses Perez <perezdi060606@gmail.com>
2025-10-14 22:56:54 -0600

    Deploy en cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,9 @@ import { defineConfig, envField } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
-import vercel from '@astrojs/vercel/serverless';
+
+import cloudflare from '@astrojs/cloudflare';
+
 
 // https://astro.build/config
 export default defineConfig({
@@ -12,11 +14,13 @@ export default defineConfig({
 
   env: {
     schema: {
-      SHOW_BUY_BUTTON: envField.boolean({access: 'public', context: 'server', default: false}),
-      SCORE_API_ENDPOINT: envField.string({access: 'public', context: 'server', default: ''}),
+      SHOW_BUY_BUTTON: envField.boolean({ access: 'public', context: 'server', default: false }),
+      SCORE_API_ENDPOINT: envField.string({ access: 'public', context: 'server', default: '' }),
       // SCORE_API_ENDPOINT: envField.string({access: 'public', context: 'client', default: ''}),
     }
   },
 
-  adapter: vercel(),
+  adapter: cloudflare({
+    imageService: 'cloudflare'
+  }),
 });
